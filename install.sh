5edd455d0089b86ed70a7fc3df74dab64dbe5133
David <dburnazyan@gmail.com>
2016-02-11 14:04:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt-get install git make gcc librados-dev autoconf automake libtool
+apt-get -y install git make gcc librados-dev autoconf automake libtool
 git clone https://github.com/akheron/jansson.git
 cd jansson/
 autoreconf -vi
